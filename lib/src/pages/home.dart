9d54c6c7330d68903174229b50bc2b7850f0dce3
Gervis Bermudez <gervisbermudez15@gmail.com>
2021-01-30 12:18:19 -0300

    build routes json based

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/providers/menu_providers.dart';
 
 class HomePage extends StatelessWidget {
   final options = ["One", "Two", "Three", "For", "Five"];
@@ -10,16 +11,35 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("App Components"),
       ),
-      body: ListView(
-        children: _createItems(),
-      ),
+      body: _list(),
     );
   }
 
-  List<Widget> _createItems() {
-    var widgetList = options.map((item) {
-      return ListTile(title: Text(item + "!"));
-    }).toList();
-    return widgetList;
+  Widget _list() {
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _createItems(snapshot.data),
+        );
+      },
+    );
+  }
+
+  List<Widget> _createItems(List<dynamic> data) {
+    final List<Widget> options = [];
+
+    data.forEach((element) {
+      final widgetTemp = ListTile(
+        title: Text(element['texto']),
+        leading: Icon(Icons.account_circle),
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () {},
+      );
+
+      options.add(widgetTemp);
+    });
+
+    return options;
   }
 }
